95549316f96a0c3b85ebf8c447146964bbd247ec
Jan Stübner <stuebnerjan@gmail.com>
2018-05-02 17:50:20 +0200

    Stadtspaziergang Wien

---
+++ wien.js
@@ -0,0 +1,81 @@
+
+let myMap = L.map("mapdiv"); //http://leafletjs.com/reference-1.3.0.html#map-l-map
+const wienGroup = L.featureGroup();
+let myLayers = {
+    osm : L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"), //http://leafletjs.com/reference-1.3.0.html#tilelayer-l-tilelayer
+        subdomains : ["a","b","c"], 
+        attribution : "Datenquelle: <a href=openstreetmap.org</a>", 
+
+    geolandbasemap : L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps","maps1","maps2","maps3","maps4"], //http://leafletjs.com/reference-1.3.0.html#tilelayer-subdomains
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>", //http://leafletjs.com/reference-1.3.0.html#layer-attribution
+    }
+),
+    bmapoverlay : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", { 
+    subdomains : ["maps","maps1","maps2","maps3","maps4"], 
+    attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    }
+),
+   
+    bmapgrau : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png",{
+    subdomains : ["maps","maps1","maps2","maps3","maps4"], 
+    attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    }
+),
+    bmaphidpi : L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg",{
+    subdomains : ["maps","maps1","maps2","maps3","maps4"], 
+    attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    }
+),
+    bmaporthofoto30cm : L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg",{
+    subdomains : ["maps","maps1","maps2","maps3","maps4"], 
+    attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    }
+),
+}
+myMap.addLayer(myLayers.geolandbasemap); //http://leafletjs.com/reference-1.3.0.html#layergroup-addlayer
+
+let myMapControl = L.control.layers({ //http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
+    "Openstreetmap" : myLayers.osm,
+    "basemap.at Grundkarte" : myLayers.geolandbasemap,
+    "basemap.at Overlay" : myLayers.bmapoverlay,
+    "basemap.at grau" : myLayers.bmapgrau,
+    "basemap.at highdpi" : myLayers.bmaphidpi,
+    "basemap. at Orthofoto" : myLayers.bmaporthofoto30cm, 
+},{
+    "basemap.at Overlay" : myLayers.bmapoverlay,
+    "Stadtspaziergang Wien": wienGroup,
+    
+},{
+    collapsed: false
+}); // Sollte eigentlich darüber gehen http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
+
+myMap.addControl(myMapControl); //http://leafletjs.com/reference-1.3.0.html#map-addcontrol
+
+myMap.setView([47.267,11.383], 11); //http://leafletjs.com/reference-1.3.0.html#map-setview
+
+
+L.control.scale({
+    imperial: false, 
+    maxWidth: 200, 
+    position: "bottomleft"
+}).addTo(myMap)
+
+// verwendete Methode: http://leafletjs.com/reference-1.3.0.html#control-scale-l-control-scale
+// verwendete Optionen: http://leafletjs.com/reference-1.3.0.html#control-scale-maxwidth
+// http://leafletjs.com/reference-1.3.0.html#control-scale-metric
+// http://leafletjs.com/reference-1.3.0.html#control-scale-position
+
+console.log("Wien Data: ",wiendata); 
+
+myMap.addLayer(wienGroup);
+let geojson = L.geoJSON(wiendata).addTo(wienGroup);
+geojson.bindPopup(function(layer) {
+        const prop = layer.feature.properties;
+        const popupText = `<h1>${prop.NAME}</h1>
+        <p>Informationen: ${prop.BEMERKUNG}</p>`;
+        return popupText;
+    
+});
+myMap.fitBounds(wienGroup.getBounds());
+
