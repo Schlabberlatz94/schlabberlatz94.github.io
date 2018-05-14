c558e35b015e367ab5c080e4f67821fd2d4be7a6
Jan Stübner <stuebnerjan@gmail.com>
2018-05-14 20:40:33 +0200

    Biketirol

---
+++ biketirol/biketirol.js
@@ -0,0 +1,84 @@
+/*
+    Vorbereitung: GPX Track herunterladen und nach GeoJSON konvertieren
+    -------------------------------------------------------------------
+    Datenquelle https://www.data.gv.at/suche/?search-term=bike+trail+tirol&searchIn=catalog
+    Download Einzeletappen / Zur Ressource ...
+    Alle Dateien im unterverzeichnis data/ ablegen
+    Die .gpx Datei der eigenen Etappe als etappe00.gpx speichern
+    Die .gpx Datei über https://mapbox.github.io/togeojson/ in .geojson umwandeln und als etappe00.geojson speichern
+    Die etappe00.geojson Datei in ein Javascript Objekt umwandeln und als etappe00.geojson.js speichern
+
+    -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
+*/
+
+// eine neue Leaflet Karte definieren
+
+// Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
+// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
+
+// Maßstab metrisch ohne inch
+
+// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
+
+// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
+// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
+
+// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
+
+// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
+
+let myMap = L.map("mapdiv"); //http://leafletjs.com/reference-1.3.0.html#map-l-map
+const wienGroup = L.featureGroup();
+let myLayers = {
+    osm : L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"), //http://leafletjs.com/reference-1.3.0.html#tilelayer-l-tilelayer
+        subdomains : ["a","b","c"], 
+        attribution : "Datenquelle: <a href=openstreetmap.org</a>", 
+
+    geolandbasemap : L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains : ["maps","maps1","maps2","maps3","maps4"], //http://leafletjs.com/reference-1.3.0.html#tilelayer-subdomains
+        attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>", //http://leafletjs.com/reference-1.3.0.html#layer-attribution
+    }
+),
+    bmapoverlay : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", { 
+    subdomains : ["maps","maps1","maps2","maps3","maps4"], 
+    attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>",
+    }
+),
+	elektronischeKarteSommer : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+		attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
+	}
+), 
+	elektronischeKarteWinter : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_base_winter/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+		attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
+	}
+), 
+	elektronischeKarteNomenklatur : L.tileLayer("http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+		attribution : "Datenquelle: <a href='https://www.kartetirol.at'>kartetirol.at</a>",
+	}
+), 
+}
+myMap.addLayer(myLayers.geolandbasemap); //http://leafletjs.com/reference-1.3.0.html#layergroup-addlayer
+
+let myMapControl = L.control.layers({ //http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
+    "Openstreetmap" : myLayers.osm,
+    "basemap.at Grundkarte" : myLayers.geolandbasemap,  
+	"Elektronische Karte Sommer" : myLayers.elektronischeKarteSommer,
+	"Elektronische Karte Winter" : myLayers.elektronischeKarteWinter,
+},{
+    "basemap.at Overlay" : myLayers.bmapoverlay,
+	"Nomenklatur Overlay" : myLayers.elektronischeKarteNomenklatur,
+        
+},{
+    collapsed: false
+}); // Sollte eigentlich darüber gehen http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
+
+myMap.addControl(myMapControl); //http://leafletjs.com/reference-1.3.0.html#map-addcontrol
+
+myMap.setView([47.14237,10.57026], 11); //http://leafletjs.com/reference-1.3.0.html#map-setview
+
+
+L.control.scale({
+    imperial: false, 
+    maxWidth: 200, 
+    position: "bottomleft"
+}).addTo(myMap)
